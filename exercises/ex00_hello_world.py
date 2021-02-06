2e09b76a07a5245ef96d42d43c77d4cdd751b102
Your Name <you@example.com>
2021-02-06 13:30:24 -0500

    Finished input exercise!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,3 @@
+"""My first program for COMP110."""
+print("Hello,world.")
+__author__ = "730393936"
---
+++ lessons/input.py
@@ -0,0 +1,4 @@
+"""A program to demonstrate user input and variables."""
+
+print ("Wow," + input("Who are you? ") + ", you rock!")
+print(input("Who are you? ") + "have a great day!")
